bd004e40ca1292955812a6cd9cbbf3b11cbf3559
George Raber <graber@gmail.com>
2015-01-23 16:03:43 -0600

    Made map div seperate div to enable cursor correct

--- js/app.js
+++ js/app.js
@@ -2,28 +2,28 @@ requirejs.config({
     appDir: ".",
     baseUrl: "js",
 	shim : {
-      //  "bootstrap" : { "deps" :['jquery'] }
+        "bootstrap" : { "deps" :['jquery'] }
     },
     paths: { 
         /* Load jquery from google cdn. On fail, load local file. */
         'jquery': ['//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min', 'libs/jquery-min'],
-        'leaflet': '//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.2/leaflet' //,
-//		"bootstrap" :  "//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min"  
+        'leaflet': '//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.2/leaflet' ,
+		"bootstrap" :  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min"  
     }
 });
 
-define(['jquery', 'leaflet'], function($, L, bs) {
+define(['jquery', 'leaflet', 'bootstrap'], function($, L, bs) {
     
 	
   	console.log("loaded");
+   
+	var MAP
    
     setup = function() {
    
 		doLayout();
 		
-		
-		var map = L.map('map', {trackResize:true, maxZoom:18}).setView([31.328920, -89.333514], 10);
-		
+		MAP = L.map('mappanel', {trackResize:true, maxZoom:18}).setView([31.328920, -89.333514], 10);
 		
 		var bwlayer = L.tileLayer(
 					'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -31,13 +31,15 @@ define(['jquery', 'leaflet'], function($, L, bs) {
 					maxZoom: 18,
 					})
 		
-		map.addLayer(bwlayer);
+		MAP.addLayer(bwlayer);
 		
 		$(".controlbutton").click(function(e){
 		
 			transitionTo(e.target.id);
 			
 		});
+		
+		transitionTo("mappanel");
 
     }
   
@@ -49,42 +51,47 @@ define(['jquery', 'leaflet'], function($, L, bs) {
 	});
 	
 	transitionTo = function(buttonClicked) {
-	
+
 			console.log("You clicked " + buttonClicked);
-			if (buttonClicked == "mapbutton") { 
-				  $( ".controlpanel" ).animate({
-					opacity: 0,
-				  }, 500, function() {
-					$( ".controlpanel" ).hide() 
-				  });
-			}
-			
-			else {
-			
-				$( ".controlpanel" ).css({"z-index": "4999"});
 				
 				panel = "#" + buttonClicked.replace("button","panel");
-				$( panel ).css({"z-index": "5000"});
+				
+				$( ".controlpanel" ).each(function( index, el ) {
+					if ( "#" + el.id != panel ) { 
+					
+						$( el ).animate({
+							opacity: 0,
+						}, 500, function() {
+							
+						});
+					
+					
+					};
+				});
+				
+				
+				//$( panel ).css({"z-index": "5000"});
 				$( panel ).css({"opacity": 0});
 				$( panel ).show();
 				
 				  $( panel ).animate({
 					opacity: 1,
 				  }, 500, function() {
-					$( ".controlpanel" ).each(function( index, el ) {
-						if ( "#" + el.id != panel ) { $(el).hide(); } ;
-					});
+					
+				//	$( ".controlpanel" ).each(function( index, el ) {
+				//		if ( "#" + el.id != panel ) { $(el).hide(); } ;
+				//	});
 				  });
 				
-			}
+		//	}
 	
 	}
 	
 	doLayout = function() {
 		mapwidth = $( window ).width() - $( "#control" ).width(); //- 5;
 		maphieght = $( window ).height() - $( "#header" ).height() - $( "#footer" ).height();
-		$( "#map" ).css({"width": mapwidth + "px"});
-		$( "#map" ).css({"height": maphieght + "px"});
+		$( "#mainpanel" ).css({"width": mapwidth + "px"});
+		$( "#mainpanel" ).css({"height": maphieght + "px"});
 		$( "#control" ).css({"height": maphieght + "px"});	
 	};
      
