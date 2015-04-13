5a3aac95f3e37de3f93ca66bdad2e14b4ed6f021
George Raber <george.raber@usm.edu>
2015-04-13 14:19:39 -0500

    Eduardo is gone

--- js/userPanel.js
+++ js/userPanel.js
@@ -17,31 +17,7 @@ define("userPanel",
 		this.div = $("#" + div);
 		this.div.empty();
 		//injectCSS(css);
-		this.div.append($(html));
-		
-		gridder = $("#grid-user").bootgrid({
-		selection: true,
-		multiSelect: true,
-		formatters: {
-        "commands": function(column, row)
-        {
-            return "<button type=\"button\" class=\"btn btn-xs btn-default command-edit\" data-row-id=\"" + row.id + "\"><span class=\"fa fa-pencil\"></span></button> " + 
-                "<button type=\"button\" class=\"btn btn-xs btn-default command-delete\" data-row-id=\"" + row.id + "\"><span class=\"fa fa-trash-o\"></span></button>";
-        }
-    }
-}).on("loaded.rs.jquery.bootgrid", function()
-{
-    /* Executes after data is loaded and rendered */
-	console.log(gridder);
-    gridder.find(".command-edit").on("click", function(e)
-    {
-        alert("You pressed edit on row: " + $(this).data("row-id"));
-    }).end().find(".command-delete").on("click", function(e)
-    {
-        alert("You pressed delete on row: " + $(this).data("row-id"));
-    });
-});
-		
+
 		//make request
 		
 		if (this.app.loggedIn > -1) {
@@ -77,7 +53,60 @@ define("userPanel",
 		
 		makeGrid: function(data) {
 		
+		this.div.append($(html));
+		
+		this.tableBody = this.div.find("tbody");
+		
+
+		
 		console.log("%%%%%%%%", data);
+		
+		insertRows = "";
+		
+		$.each(data.data, $.proxy( function(i,d) {
+
+			nrow = "<tr><td>" + d.qid + "</td><td>" + d.question + "</td><td>" + d.created + "</td></tr>"			
+			insertRows = insertRows + nrow;
+			
+		}, this));
+		
+	
+		this.tableBody.append($(insertRows));
+		
+		
+		
+		gridder = $("#grid-user").bootgrid({
+		selection: true,
+		multiSelect: true,
+		formatters: {
+        "commands": function(column, row)
+        {
+            return "<button type=\"button\" class=\"btn btn-xs btn-default command-edit\" data-row-id=\"" + row.id + "\"><span class=\"fa fa-pencil\"></span></button> " + 
+                "<button type=\"button\" class=\"btn btn-xs btn-default command-delete\" data-row-id=\"" + row.id + "\"><span class=\"fa fa-trash-o\"></span></button>";
+        }
+    }
+	}).on("loaded.rs.jquery.bootgrid", function()
+	{
+		/* Executes after data is loaded and rendered */
+		console.log(gridder);
+		gridder.find(".command-edit").on("click", function(e)
+		{
+			alert("You pressed edit on row: " + $(this).data("row-id"));
+		}).end().find(".command-delete").on("click", function(e)
+		{
+			alert("You pressed delete on row: " + $(this).data("row-id"));
+		});
+	}).on("selected.rs.jquery.bootgrid", function(e, rows)
+	{
+		var rowIds = [];
+		for (var i = 0; i < rows.length; i++)
+		{
+			rowIds.push(rows[i].id);
+		}
+		alert("Select: " + rowIds.join(","));
+	});
+		
+		this.grid = gridder;
 		
 		}
      
